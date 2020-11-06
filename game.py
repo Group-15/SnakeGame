5317f35bcc737ecdc46109c8f24cf2b2a0b12d66
AngelMaciel23 <amaciel1@student.gsu.edu>
2020-11-06 01:02:37 -0500

    Class

---
+++ game.py
@@ -0,0 +1,14 @@
+from config import Config
+from snake import Snake
+from apple import Apple
+import pygame
+
+class Game():
+    def __int__(self):
+        pygame.init()
+        self.screen  = pygame.display.set_mode((Config.WINDOW_WIDTH, Config.WINDOW_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+        pygame.display.set_caption('Snake')
+        self.apple = Apple()
+        self.snake = Snake()
